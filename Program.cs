84696ea6c49c20b5d95366ae1f16c61bba3e9f9f
Nadusha <nadusha230275@gmail.com>
2022-07-04 09:46:38 +0500

    работает

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// показать вторую цифру трех значного числа
+Console.WriteLine("введите трехзначное число");
+int a= int.Parse(Console.ReadLine());
+if (a>99 && a<1000)
+{
+    Console.WriteLine("вторая цифра числа: ");
+    Console.Write ((a/10) % 10);
+}
+else
+{
+    Console.WriteLine(" число не является трехзначным ");
+}
+
